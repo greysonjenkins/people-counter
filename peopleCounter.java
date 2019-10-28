d5ef785cd6170001d30f9239ace08b4a621bbd0d
Greyson Jenkins <greyjenkins@live.com>
2019-10-28 00:59:40 -0700

    Standardized syntax

--- peopleCounter.java
+++ peopleCounter.java
@@ -9,38 +9,41 @@ BOILERPLATE CODE; still need to fill in placeholder arguments
 #define TOKEN "..." // Ubidots TOKEN
 #define WIFINAME "..." // SSID
 #define WIFIPASS "..." // Wifi Password
-#define DEVICE "pir-sensor" // Assign  device label
-#define VARIABLE "motion" // Assign  variable label
+#define DEVICE "pir-sensor" // Assign device label
+#define VARIABLE "motion" // Assign variable label
 #define LED 2
 #define SENSOR D6
 
-uint8_t counter=0;
+uint8_t counter = 0;
 unsigned long state = 0;
 
 Ubidots client(TOKEN);
 
 // Auxiliar functions
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte* payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i=0;i<length;i++) {
+  for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 }
 
 // Main functions
-void setup() {
+void setup()
+{
   // Setup code here, to run once:
   Serial.begin(115200);
   pinMode(SENSOR, INPUT);
   pinMode(LED, OUTPUT);
   client.wifiConnection(WIFINAME, WIFIPASS);
   client.begin(callback);
-  }
+}
 
-void loop() {
+void loop()
+{
   // Main code here, to run repeatedly:
   if (!client.connected()) {
       digitalWrite(LED, LOW);
@@ -53,14 +56,14 @@ void loop() {
   uint8_t sensorValue = digitalRead(SENSOR);
   bool flag = false;
 
-  if(sensorValue>0){
-    for(uint8_t wait=0; wait<=4; wait++){
+  if (sensorValue > 0) {
+    for (uint8_t wait = 0; wait <= 4; wait++) {
       sensorValue = digitalRead(SENSOR);
       Serial.println(sensorValue);
-      if(sensorValue==1){
+      if (sensorValue == 1) {
       counter++;
       }
-      if(counter>3){
+      if (counter > 3) {
         flag = true;
       }
       delay(500);
@@ -69,13 +72,13 @@ void loop() {
 
   Serial.println("sending data");
   uint8_t value;
-  if(flag){
+  if (flag) {
     value = 1;
     client.add(VARIABLE, value);
     client.ubidotsPublish(DEVICE);
-  }else{
+  } else {
     value = 0;
-    if(state == 10000){
+    if (state == 10000) {
       client.add(VARIABLE, value);
       client.ubidotsPublish(DEVICE);
       state = 0;
